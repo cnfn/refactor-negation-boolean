ebf5c6a4661a7ad0ae30f40c8e1f3b1ae5b161cc
Cnfn <cnfnss@gmail.com>
2021-08-05 17:59:58 +0800

    refactor(BooleanOperatorAction): java -> kotlin

--- src/main/java/BooleanOperatorAction.kt
+++ src/main/java/BooleanOperatorAction.kt
@@ -1,77 +1,59 @@
-import java.util.Objects;
-
-import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
-import com.intellij.codeInspection.util.IntentionFamilyName;
-import com.intellij.codeInspection.util.IntentionName;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.JavaTokenType;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiExpression;
-import com.intellij.psi.PsiMethodCallExpression;
-import com.intellij.psi.PsiParenthesizedExpression;
-import com.intellij.psi.PsiPrefixExpression;
-import com.intellij.psi.SmartPointerManager;
-import com.intellij.psi.SmartPsiElementPointer;
-import com.intellij.util.IncorrectOperationException;
-import org.apache.commons.lang3.BooleanUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.codeInspection.util.IntentionFamilyName
+import com.intellij.codeInspection.util.IntentionName
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.*
+import org.apache.commons.lang3.BooleanUtils
 
 /**
  * @author zhixiao.mzx
  * @date 2021/08/02
  */
-public class BooleanOperatorAction extends PsiElementBaseIntentionAction {
-    public static final String ACTION_HINT_TEXT = "BooleanOperatorAction: getText";
+class BooleanOperatorAction : PsiElementBaseIntentionAction() {
+    private var prefixExpPointer: SmartPsiElementPointer<PsiElement>? = null
 
-    private SmartPsiElementPointer<PsiElement> prefixExpPointer;
-
-    @Override
-    public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element)
-        throws IncorrectOperationException {
-        String exp = "BooleanUtils.isFalse(a)";
-        PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
-        PsiMethodCallExpression psiNewExpression = (PsiMethodCallExpression)factory.createExpressionFromText(exp, null);
-
-        PsiPrefixExpression prefixExpression = (PsiPrefixExpression)prefixExpPointer.getElement();
-        PsiExpression operand = Objects.requireNonNull(prefixExpression.getOperand());
-        if (operand instanceof PsiParenthesizedExpression) {
-            operand = ((PsiParenthesizedExpression)operand).getExpression();
-            assert operand != null;
+    override fun invoke(project: Project, editor: Editor, element: PsiElement) {
+        val prefixExpression = prefixExpPointer!!.element as PsiPrefixExpression
+        var operand = prefixExpression.operand!!
+        if (operand is PsiParenthesizedExpression) {
+            operand = operand.expression!!
         }
 
-        psiNewExpression.getArgumentList().getExpressions()[0].replace(operand);
-        prefixExpression.replace(psiNewExpression);
+        val exp = "BooleanUtils.isFalse(a)"
+        val factory = JavaPsiFacade.getElementFactory(project)
+        val psiNewExpression = factory.createExpressionFromText(exp, null) as PsiMethodCallExpression
+        psiNewExpression.argumentList.expressions[0].replace(operand)
+        prefixExpression.replace(psiNewExpression)
     }
 
-    @Override
-    public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
-        PsiElement context = element.getContext();
-
-        if (BooleanUtils.isFalse(context instanceof PsiPrefixExpression)) {
-            context = element.getParent().getContext();
+    override fun isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean {
+        var context = element.context
+        if (context !is PsiPrefixExpression) {
+            context = element.parent.context
         }
-        assert context != null;
-        if (context instanceof PsiPrefixExpression) {
-            PsiPrefixExpression prefixExpression = (PsiPrefixExpression)context;
-            if (JavaTokenType.EXCL.equals(prefixExpression.getOperationSign().getTokenType())) {
-                prefixExpPointer =
-                    SmartPointerManager.getInstance(project).createSmartPsiElementPointer(prefixExpression);
-                return true;
-            }
+
+        if (context !is PsiPrefixExpression) {
+            return false
         }
-        return false;
+
+        val prefixExpression = context
+        if (JavaTokenType.EXCL == prefixExpression.operationSign.tokenType) {
+            prefixExpPointer = SmartPointerManager.getInstance(project).createSmartPsiElementPointer(prefixExpression)
+            return true
+        }
+        return false
     }
 
-    @Override
-    public @NotNull @IntentionFamilyName String getFamilyName() {
-        return "BooleanOperatorAction: getFamilyName";
+    override fun getFamilyName(): @IntentionFamilyName String {
+        return "BooleanOperatorAction: getFamilyName"
     }
 
-    @Override
-    public @IntentionName @NotNull String getText() {
-        return ACTION_HINT_TEXT;
+    override fun getText(): @IntentionName String {
+        return ACTION_HINT_TEXT
+    }
+
+    companion object {
+        const val ACTION_HINT_TEXT = "BooleanOperatorAction: getText"
     }
 }
